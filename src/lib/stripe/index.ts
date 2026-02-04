fa6ca2e62c36029de572edf86b587024f025a72b
sullygpt <sullygpt@gmail.com>
2026-02-04 07:09:45 -0600

    Fix Stripe apiVersion

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -8,7 +8,6 @@ export function getStripe(): Stripe {
       throw new Error('STRIPE_SECRET_KEY is not configured');
     }
     _stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2025-12-15.clover',
       typescript: true,
     });
   }
